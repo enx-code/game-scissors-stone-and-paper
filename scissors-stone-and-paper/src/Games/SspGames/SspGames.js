930c6097508a0978902ec6b98bc088f6d53be6b0
Enky <enkh6at@yahoo.com>
2022-12-09 14:59:43 +0000

    not finished still working on it

--- scissors-stone-and-paper/src/Games/SspGames/SspGames.js
+++ scissors-stone-and-paper/src/Games/SspGames/SspGames.js
@@ -1,37 +1,71 @@
-import { useState } from "react";
-import './SspGames.css'
+import { useState, useEffect } from "react";
+import "./SspGames.css";
 
 export default function SspGames({ player }) {
-  const [points, setPoints] = useState(0);
-  const [mySelection, setMyselection] = useState("")
-
+  const [score, setScore] = useState(0);
+  const [appScore, setAppScore] = useState(0);
+  const [userSelection, setMyselection] = useState("Rock");
+  const [appChoice, setAppChoice] = useState("Rock");
+  const [result, setResult] = useState(null);
 
   const num = Math.floor(Math.random() * 9);
-  let result = "";
-  if (num >= 0 && num <= 2) {
-    result = "scissors";
-  } else if (num >= 3 && num <= 5) {
-    result = "stone";
-  } else {
-    result = "paper";
-  }
-//   return result;
-  function startGame(result, mySelection){
-    if ("scissors" === result) {
-        console.log("draw")
-    } else (console.log("working"))
-  }
- 
-  console.log("state", mySelection, result)
-  
+  useEffect(() => {
+    let result = "";
+    if (num >= 0 && num <= 2) {
+      result = "Scissors";
+    } else if (num >= 3 && num <= 5) {
+      result = "Rock";
+    } else {
+      result = "Paper";
+    }
+    setAppChoice(result);
+  }, [userSelection]);
 
+  console.log("Choices App", appChoice, "MyChoice", userSelection);
+
+  const startGame = (appChoice, userSelection) => {
+    let result = "";
+    if (appChoice === userSelection) {
+      result = "Draw";
+    }
+    if (userSelection === "Rock") {
+      if (appChoice === "Paper") {
+        result = "App, won!";
+      }
+    }
+    setResult(result);
+  };
+  console.log("app won", result)
   return (
     <div>
-      <h1>hello random Sign! {result}</h1>
-      <button type="button" value={"scissors"} onClick={(e)=>{setMyselection(e.target.value)}}>Scissors</button>
-      <button onClick={startGame()}>Stone</button>
-      <button>Paper</button>
-      <p>{points}</p>
+      <h1>Hello Player {player}!</h1>
+      <h2>App Choice, {appChoice}.</h2>
+      <h2></h2>
+      <p>{score}</p>
+      <button
+        onClick={() => {
+          setMyselection("Scissors");
+        }}
+      >
+        Scissors
+      </button>
+      <button
+        onClick={() => {
+          setMyselection("Rock");
+        }}
+      >
+        Rock
+      </button>
+      <button
+        onClick={() => {
+          setMyselection("Paper");
+        }}
+      >
+        Paper
+      </button>
+      <h2>
+        {player}'s choice {userSelection}!
+      </h2>
     </div>
   );
 }
